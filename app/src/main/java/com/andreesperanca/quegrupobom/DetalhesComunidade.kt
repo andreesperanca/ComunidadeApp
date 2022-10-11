f37bc3a216e8af3d39c9a531d2b992d3a4a1202f
andreesperanca <andreluizesperancacorreia@gmail.com>
2022-10-11 20:34:46 -0300

    feat: create details layout

--- app/src/main/java/com/andreesperanca/quegrupobom/DetalhesComunidade.kt
+++ app/src/main/java/com/andreesperanca/quegrupobom/DetalhesComunidade.kt
@@ -2,6 +2,8 @@ package com.andreesperanca.quegrupobom
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.andreesperanca.quegrupobom.databinding.FragmentDetalhesComunidadeBinding
 import com.andreesperanca.quegrupobom.util.generics.BaseFragment
 
@@ -10,10 +12,22 @@ class DetalhesComunidade : BaseFragment<FragmentDetalhesComunidadeBinding>(
     FragmentDetalhesComunidadeBinding::inflate
 ) {
 
+    private val args: DetalhesComunidadeArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.ivComunidadeBanner.setImageDrawable(args.comunidade.image)
+        binding.tvNomeComunidade.text = "Comunidade dos nerds"
+        binding.tvRedesSociaisTitulo.text = "Redes sociais"
+        binding.tvDescricaoTitulo.text = "Descrição grupo"
+        binding.tvDescricaoComunidade.text = "aqui tem uma descricao bem bonita"
+        binding.tvRegrasComunidadeTitle.text = "Regras comunidade"
+        binding.tvRegrasComunidade.text = "Aqui tem as regras bem bonitas"
     }
 
     override fun configureToolbar() {
-
+        binding.comunidadeToolbar.apply {
+            setNavigationIcon(R.drawable.ic_seta_esquerda_branca)
+            setNavigationOnClickListener { findNavController().popBackStack() }
+        }
     }
 }
